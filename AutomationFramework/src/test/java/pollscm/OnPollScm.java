b562fad428e80d412ce6281326f94e48a047c1f4
juhis <juhis@JuhiSingh_1511>
2024-11-20 13:57:09 +0530

    Onpollscm executed

---
+++ AutomationFramework/src/test/java/pollscm/OnPollScm.java
@@ -0,0 +1,12 @@
+package pollscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnPollScm {
+	@Test
+	public void onPollScm() {
+		Reporter.log("Onpollscm executed",true);
+	}
+
+}
